1e7b7831d4f05ea0b493306dd1460297b2d8f84c
HeadYak <franksu@y7mail.com>
2021-12-27 22:53:23 +1100

    Code to take in two numbers and print sum

---
+++ variables/addition.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var num1 int
+	var num2 int
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&num1)
+
+	fmt.Print("Enter second number: ")
+	fmt.Scan(&num2)
+
+	fmt.Println(add(num1, num2))
+
+}
+
+func add(x int, y int) int {
+	return x + y
+}
